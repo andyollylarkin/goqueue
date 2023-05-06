237abaa4e24837e0ee5f4d24440c73c116d498ad
Denis Davydov <follenlast2@gmail.com>
2023-05-06 14:15:40 +0300

    gitignore

--- cmd/goqueue/main.go
+++ cmd/goqueue/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"flag"
-	"goqueue/configs"
+	"fmt"
+	application "goqueue/internal/app"
+	"goqueue/internal/configs"
+	"log"
+	"time"
+)
+
+var (
+	Version   = "dev"
+	BuildTime = time.Now().Format("2006-01-02 15:04:05")
 )
 
 func main() {
+	fmt.Printf("Current build version: %s\nBuild time: %s\n", Version, BuildTime)
 	dbgMode := flag.Bool("debug", false, "debug mode")
 	flag.Parse()
-	_ = configs.New(*dbgMode)
+	config := configs.MustNewConfig(*dbgMode)
+	app := application.NewApplication(config)
+	err := app.Start()
+	if err != nil {
+		log.Fatalf("Application error: %s", err.Error())
+	}
 }
